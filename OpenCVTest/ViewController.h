2559d54679220f5840f4589292a7ef572080897d
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2017-07-27 11:31:47 +0900

    Initial commit

---
+++ OpenCVTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenCVTest
+//
+//  Created by Chihiro on 2017/07/27.
+//  Copyright © 2017 chihiroshima. All rights reserved.
+//
+
+#ifndef ViewController_h
+#define ViewController_h
+
+
+#endif /* ViewController_h */
---
+++ OpenCVTest/ViewController.mm
@@ -0,0 +1,9 @@
+//
+//  ViewController.m
+//  OpenCVTest
+//
+//  Created by Chihiro on 2017/07/27.
+//  Copyright © 2017 chihiroshima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
